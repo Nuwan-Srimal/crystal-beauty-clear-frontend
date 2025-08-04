e6b4b6835bac5de5643a58898d8d0d5f514234ea
Nuwan <nuwan79723@gmail.com>
2025-08-04 20:44:25 +0530

    add new product for test

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
 
     <ProductCard name="Apple iPad" price="$499" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/xlarge/ipad_pro_f1345e8ae.png"/>
 
+    <ProductCard name="Apple Watch" price="$399" image="https://www.apple.com/v/watch/br/images/overview/select/product_se__c83w8hz9gre6_large.png"/>
+    
     <ProductCard name="Mac Book Pro" price="$1299" image="https://www.apple.com/assets-www/en_WW/mac/product_tile/xlarge/mba_13_15_2fb165671.png"/>
     </>
   )
