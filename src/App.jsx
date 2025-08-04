9b147e7c7cc0cd249d566bd155493918db1fe65d
Nuwan <nuwan79723@gmail.com>
2025-08-04 20:40:04 +0530

    crystal-beauty-clear-frontend

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <ProductCard name="Apple iPad" price="$499" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/xlarge/ipad_pro_f1345e8ae.png"/>
+
+    <ProductCard name="Mac Book Pro" price="$1299" image="https://www.apple.com/assets-www/en_WW/mac/product_tile/xlarge/mba_13_15_2fb165671.png"/>
+    </>
+  )
+}
+
+export default App
